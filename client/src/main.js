207a550aca7cef7efaba224807af819fa5766f1b
Parker Sprouse <parker@parkersprouse.me>
2018-12-03 21:21:38 -0600

    Bootstrapped client and start playing around

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+import './assets/css/bootstrap.min.css';
+import './assets/css/styles.scss';
+
+import Vue from 'vue';
+import NumberInput from './components/number-input.vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.component('number-input', NumberInput);
+
+Vue.config.productionTip = false;
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
---
+++ client/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style='width: 50%; margin-left: auto; margin-right: auto;'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+};
+</script>
