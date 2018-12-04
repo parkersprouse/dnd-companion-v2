589a2d74dbf19490d5136be2d03f0f468f7e32b3
Parker Sprouse <parker@parkersprouse.me>
2018-12-03 22:14:07 -0600

    Added EventBus

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import './assets/css/bootstrap.min.css';
 import './assets/css/styles.scss';
 
 import Vue from 'vue';
-import NumberInput from './components/number-input.vue';
+import NumberInput from './components/NumberInput.vue';
 import App from './App.vue';
 import router from './router';
 
