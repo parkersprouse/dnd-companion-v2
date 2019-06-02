be28fb1ea91564998ed7dbc84bfb23e95ef187a3
Parker Sprouse <parker@parkersprouse.me>
2019-06-02 03:32:50 -0500

    Got the layout for the armor page basically done

--- client2/src/views/info/ArmorList.vue
+++ client2/src/views/info/ArmorList.vue
@@ -1,14 +1,47 @@
 <template>
   <v-layout>
-    <v-flex sm12 md6 offset-md3>
-      <v-data-table :headers="headers" :items="desserts" class="elevation-1">
-        <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="text-xs-right">{{ props.item.iron }}</td>
+    <v-flex sm12 md8 offset-md2>
+
+      <div id='filter-controls'>
+        <div>
+          <v-text-field v-model='filter_name' clearable label='Filter'
+                        prepend-inner-icon='fa-search'></v-text-field>
+        </div>
+        <div>
+          <div>
+            <v-select v-model='filter_category' :items='filter_category_opts' label='Armor Category'
+                      menu-props='offsetY'></v-select>
+          </div>
+          <div>
+            <v-select v-model='filter_strength' :items='filter_strength_opts'
+                      label='Minimum Strength' menu-props='offsetY'></v-select>
+          </div>
+          <div>
+            <v-select v-model='filter_stealth' :items='filter_stealth_opts'
+                      label='Stealth Disadvantage' menu-props='offsetY'></v-select>
+          </div>
+        </div>
+      </div>
+
+      <v-data-table v-if='filtered_armor' class='elevation-1' :headers='headers' hide-actions
+                   :items='filtered_armor' must-sort :search='filter_name'
+                    sort-icon='fa-arrow-up mr-2'>
+        <template v-slot:items='props'>
+          <td class='text-xs-center'>
+            {{ props.item.name }}
+          </td>
+          <td class='text-xs-center'>
+            {{ props.item.armor_category }}
+          </td>
+          <td class='text-xs-center'>
+            {{ armorClass(props.item.armor_class) }}
+          </td>
+          <td class='text-xs-center'>
+            {{ props.item.str_minimum || '-' }}
+          </td>
+          <td class='text-xs-center'>
+            {{ props.item.stealth_disadvantage ? 'Disadvantage' : '-' }}
+          </td>
         </template>
       </v-data-table>
     </v-flex>
@@ -16,11 +49,18 @@
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'armor_list',
   data() {
     return {
       armor: null,
+      filter_name: '',
+      filter_category: '',
+      filter_stealth: '',
+      filter_strength: '',
+      filtered_armor: null,
     };
   },
   mounted() {
@@ -34,5 +74,117 @@ export default {
         this.error = true;
       });
   },
+  methods: {
+    armorClass(obj) {
+      const dex_modifier = obj.dex_bonus ? ' + dex. modifier' : '';
+      const max_bonus = obj.max_bonus ? ` (max ${obj.max_bonus})` : '';
+      return `${obj.base} ${dex_modifier} ${max_bonus}`;
+    },
+    filter() {
+      let filtered = _.cloneDeep(this.armor);
+
+      if (this.filter_category) {
+        filtered = _.filter(filtered, { armor_category: this.filter_category });
+      }
+
+      if (this.filter_stealth) {
+        filtered = _.filter(filtered, (armor) => {
+          if (this.filter_stealth === 'Yes') return armor.stealth_disadvantage;
+          return !armor.stealth_disadvantage;
+        });
+      }
+
+      if (this.filter_strength) {
+        filtered = _.filter(filtered, (armor) => {
+          if (this.filter_strength === 'Yes') return armor.str_minimum > 0;
+          return armor.str_minimum === 0;
+        });
+      }
+
+      this.filtered_armor = _.cloneDeep(filtered);
+    },
+  },
+  computed: {
+    filter_category_opts() {
+      return [
+        { text: 'All', value: '' },
+        { text: 'Light', value: 'Light' },
+        { text: 'Medium', value: 'Medium' },
+        { text: 'Heavy', value: 'Heavy' },
+        { text: 'Shield', value: 'Shield' },
+      ];
+    },
+    filter_stealth_opts() {
+      return [
+        { text: 'All', value: '' },
+        { text: 'Yes', value: 'Yes' },
+        { text: 'No', value: 'No' },
+      ];
+    },
+    filter_strength_opts() {
+      return [
+        { text: 'All', value: '' },
+        { text: 'Yes', value: 'Yes' },
+        { text: 'No', value: 'No' },
+      ];
+    },
+    headers() {
+      return [
+        { text: 'Name', value: 'name', align: 'center' },
+        { text: 'Category', value: 'armor_category', align: 'center' },
+        { text: 'Armor Class', value: 'armor_class.base', align: 'center' },
+        { text: 'Min. Strength', value: 'str_minimum', align: 'center' },
+        { text: 'Stealth', value: 'stealth_disadvantage', align: 'center' },
+      ];
+    },
+  },
+  watch: {
+    filter_category() {
+      this.filter();
+    },
+    filter_stealth() {
+      this.filter();
+    },
+    filter_strength() {
+      this.filter();
+    },
+  },
 };
 </script>
+
+<style lang='scss' scoped>
+#filter-controls {
+  display: flex;
+  flex-wrap: wrap;
+
+  > div:first-child,
+  > div:last-child {
+    width: 100%;
+
+    > div {
+      width: 100%;
+    }
+  }
+
+  > div:last-child {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  @media screen and (min-width: 768px) {
+    > div:first-child {
+      width: 50%;
+    }
+
+    > div:last-child {
+      width: 50%;
+
+      > div {
+        width: 33.33%;
+        width: calc(100% / 3);
+        padding-left: 1rem;
+      }
+    }
+  }
+}
+</style>
