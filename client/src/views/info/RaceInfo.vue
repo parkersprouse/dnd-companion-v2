3a8b2f381daba68f01232efb0943d5f11c608230
parkersprouse <parker@parkersprouse.me>
2019-06-09 16:51:06 -0500

    Added stylings for the race info page

--- client/src/views/info/RaceInfo.vue
+++ client/src/views/info/RaceInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id='info-page'>
     <!-- Error Alert -->
     <v-alert v-if='error' :value='true' type='error'>
       Failed to load race
@@ -12,11 +12,67 @@
 
     <div v-else>
       <h1 class='page-title'>{{ race.name }}</h1>
+
+      <div class='details-info-row thirds'>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Size</div>
+          <div class='details-info-entry-content'>
+            <div>{{ race.size }}</div>
+            <div>{{ race.size_description }}</div>
+          </div>
+        </div>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Alignment</div>
+          <div class='details-info-entry-content'>
+            <div>{{ race.alignment }}</div>
+          </div>
+        </div>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Age</div>
+          <div class='details-info-entry-content'>
+            <div>{{ race.age }}</div>
+          </div>
+        </div>
+      </div>
+
+      <div class='details-info-row thirds'>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Languages</div>
+          <div class='details-info-entry-content'>
+            <div v-if='race.languages.length === 0'>None</div>
+            <div>{{ default_languages }}</div>
+            <div>{{ race.language_desc }}</div>
+            <div v-if='race.language_options.from'>
+              <div>Select {{ race.language_options.choose }} from:</div>
+              <div v-for='race in race.language_options.from'>
+                {{ race.name }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Traits</div>
+          <div class='details-info-entry-content'>
+            <div v-if='race.traits.length === 0'>None</div>
+            <div v-for='trait in race.traits'>{{ trait.name }}</div>
+          </div>
+        </div>
+        <div class='details-info-entry'>
+          <div class='details-info-entry-title'>Subraces</div>
+          <div class='details-info-entry-content'>
+            <div v-if='race.subraces.length === 0'>None</div>
+            <div v-for='race in race.subraces'>{{ race.name }}</div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+
 export default {
   name: 'race_info',
   data() {
@@ -33,6 +89,11 @@ export default {
       .catch(() => {
         this.error = true;
       });
+  },
+  computed: {
+    default_languages() {
+      return _.join(_.map(this.race.languages, l => l.name), ', ');
+    },
   },
 };
 </script>
