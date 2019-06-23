38b65efa83919c2b597e4452e0eec86082d40a19
Parker Sprouse <parker@parkersprouse.me>
2019-06-22 19:04:26 -0500

    Began working on the class info page

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -10,8 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    camelcase: 'off',
+    'camelcase': 'off',
     'quote-props': 'off',
+    'prefer-destructuring': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
--- client/src/views/info/ClassInfo.vue
+++ client/src/views/info/ClassInfo.vue
@@ -1,10 +1,19 @@
 <template>
-  <div v-if='klass'>
-    <div v-for='(value, key) in klass' style='margin-bottom: 1rem;'>
-      {{ key }} === {{ value }}
+  <div id='info-page'>
+    <!-- Error Alert -->
+    <v-alert v-if='error' :value='true' type='error'>
+      Failed to load class
+    </v-alert>
+
+    <!-- Loading Indicator -->
+    <div v-else-if='!klass' class='text-xs-center'>
+      <v-progress-circular indeterminate :size='70' :width='7'></v-progress-circular>
+    </div>
+
+    <div v-else>
+      <h1 class='page-title'>{{ klass.name }}</h1>
     </div>
   </div>
-  <div v-else></div>
 </template>
 
 <script>
@@ -12,7 +21,9 @@ export default {
   name: 'class_info',
   data() {
     return {
+      error: false,
       klass: null,
+      shown_item: null,
     };
   },
   mounted() {
