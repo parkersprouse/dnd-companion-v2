983196da5e428326fedd10964cdeafd30c4782bf
Parker Sprouse <parker@parkersprouse.me>
2020-01-14 00:41:36 -0600

    Some more home page updates

--- client/src/components/index/LoginForm.vue
+++ client/src/components/index/LoginForm.vue
@@ -1,12 +1,14 @@
 <template>
   <form class='form-v' @submit.prevent='submit'>
-    <label class='form-v__label'>Username</label>
-    <input v-model='username' class='input form-v__input' type='text' />
+    <label class='form-v__label' for='username'>Username</label>
+    <input v-model='username' class='input form-v__input' id='username' type='text' />
 
-    <label class='form-v__label'>Password</label>
-    <input v-model='password' class='input form-v__input' type='password' />
+    <label class='form-v__label' for='password'>Password</label>
+    <input v-model='password' class='input form-v__input' id='password' type='password' />
 
-    <button class='btn' type='submit'>Login</button>
+    <button class='btn btn--primary w-full' :class='{ loading }' :disabled='loading' type='submit'>
+      Login
+    </button>
   </form>
 </template>
 
@@ -15,13 +17,23 @@ export default {
   name: 'login_form',
   data() {
     return {
+      loading: false,
       username: '',
       password: '',
     };
   },
   methods: {
     submit() {
-
+      if (this.loading) return;
+      this.loading = true;
+      this.$http.post('/api/login', { username: this.username, password: this.password })
+        .then(() => {
+          window.location.reload();
+        })
+        .catch((err) => {
+          console.log(err.response.data.message);
+          this.loading = false;
+        });
     },
   },
 };
