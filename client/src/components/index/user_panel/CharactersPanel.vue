3b6cb5ee582542dbf49713ce4ed9a42e57aa323a
Parker Sprouse <parker@parkersprouse.me>
2020-01-17 00:22:47 -0600

    Added create buttons and slightly changed color scheme

--- client/src/components/index/user_panel/CharactersPanel.vue
+++ client/src/components/index/user_panel/CharactersPanel.vue
@@ -5,15 +5,24 @@
     </div>
 
     <div class='box'>
-      <h1 class='font-semibold text-3xl mb-6 border-b'>Characters</h1>
+      <div class='flex flex-wrap justify-between mb-6 border-b'>
+        <div class=''>
+          <h1 class='font-semibold text-3xl self-end'>Characters</h1>
+        </div>
+        <div class='self-center'>
+          <router-link class='btn btn--primary' :to='{ name: "create_character" }'>
+            <i class='fas fa-plus-square mr-2'></i>Create Character
+          </router-link>
+        </div>
+      </div>
       <alert v-if='error' type='error'>{{ error }}</alert>
       <div v-else-if='loading'>
         Loading...
       </div>
       <div v-else>
         <router-link v-for='char in characters' :key='char.id'
-                     class='block box mb-3 hover:border-red-700 hover:text-red-700'
-                    :to='`characters/${char.data.id}`'>
+                     class='block box mb-3 hover:border-purple-700 hover:text-purple-700'
+                    :to='{ name: "show_character", params: { id: char.data.id } }'>
           <div>
             <span class='block font-semibold text-2xl mb-2'>
               {{ char.data.name }}
@@ -22,7 +31,7 @@
               Level {{ char.data.level }}
               {{ char.data.race || 'No Race' }} {{ char.data.class || 'No Class' }}
             </span>
-            <span v-if='char.game'>{{ char.game.name }}</span>
+            <span v-if='char.game'>In Game: {{ char.game.name }}</span>
             <span v-else class='italic'>Not in a game</span>
           </div>
         </router-link>
--- client/src/components/index/user_panel/GamesPanel.vue
+++ client/src/components/index/user_panel/GamesPanel.vue
@@ -4,7 +4,16 @@
       <a class='link' @click='$emit("updateRole", "player");'>Switch to Player</a>
     </div>
     <div class='box'>
-      <h1 class='font-semibold text-3xl mb-6 border-b'>Games</h1>
+      <div class='flex flex-wrap justify-between mb-6 border-b'>
+        <div class=''>
+          <h1 class='font-semibold text-3xl self-end'>Games</h1>
+        </div>
+        <div class='self-center'>
+          <router-link class='btn btn--primary' :to='{ name: "create_game" }'>
+            <i class='fas fa-plus-square mr-2'></i>Create Game
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
