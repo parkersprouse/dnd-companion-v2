3dc08b672781263ad904372d0db51e885b279f34
Parker Sprouse <parker@parkersprouse.me>
2020-01-20 23:55:27 -0600

    Added logic for navbar dropdown

--- client/src/components/navbar/DesktopNavbar.vue
+++ client/src/components/navbar/DesktopNavbar.vue
@@ -5,10 +5,12 @@
       <router-link class='navbar__link' :to='{ name: "index" }'>
         D&D Companion
       </router-link>
-      <div class='navbar__link'>
-        Info
-        <div>
-
+      <div class='navbar__link navbar__dropdown'>
+        <div class='navbar__dropdown__trigger'>Info</div>
+        <div class='navbar__dropdown__content'>
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
         </div>
       </div>
     </div>
@@ -39,10 +41,48 @@ export default {
   }
 
   .navbar__link {
-    @apply p-4;
+    @apply p-4 cursor-pointer;
 
     &:hover {
       background-color: rgba(167,182,194,.3);
+    }
+  }
+
+  .navbar__dropdown {
+    padding: 0;
+    position: relative;
+
+    &:hover {
+      .navbar__dropdown__content {
+        display: block;
+      }
+    }
+
+    .navbar__dropdown__trigger {
+      @apply p-4;
+      font-size: 16px;
+      width: 100%;
+      border: none;
+    }
+
+    .navbar__dropdown__content {
+      display: none;
+      position: absolute;
+      background-color: #f1f1f1;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+      z-index: 1;
+
+      a {
+        &:hover {
+          background-color: #ddd;
+        }
+
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+      }
     }
   }
 }
